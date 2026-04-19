667e2c10ec9596f10d7e5350b3f5bc47ac931a19
Fredrik Nylén <fredrik.nylen@umu.se>
2026-04-19 20:35:19 +0200

    Revised and centralized color scheme

---
+++ libs/ngx-components/src/lib/obj/octra-colors.ts
@@ -0,0 +1,10 @@
+export const OCTRA_COLORS = {
+  waveformBackground: '#EABAB9',
+  surfaceBackground: '#F1EFE4',
+  accentGreen: '#73A790',
+  accentGreenDark: '#5B8E8A',
+  textPrimary: '#2A4765',
+  accentError: '#B87D5E',
+  accentWarning: '#D7B17C',
+} as const;
+
